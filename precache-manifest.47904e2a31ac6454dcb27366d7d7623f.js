88ff9a4393f56ea25a3c6a648f1fe63ff2ee732d
RavenVol <r.volianiuk@gmail.com>
2019-11-18 11:21:50 +0200

    Updates

---
+++ precache-manifest.47904e2a31ac6454dcb27366d7d7623f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89dfab6bf33efd4d203b4b3511f6d25",
+    "url": "/Trello/index.html"
+  },
+  {
+    "revision": "6e134b9ddd515d7bafb6",
+    "url": "/Trello/static/css/main.4f544f1f.chunk.css"
+  },
+  {
+    "revision": "0ed7fae67c0d2dee704b",
+    "url": "/Trello/static/js/2.1d439d66.chunk.js"
+  },
+  {
+    "revision": "6e134b9ddd515d7bafb6",
+    "url": "/Trello/static/js/main.60c3c8a1.chunk.js"
+  },
+  {
+    "revision": "087c7e708b2204f3b9c9",
+    "url": "/Trello/static/js/runtime~main.10431659.js"
+  }
+]);
